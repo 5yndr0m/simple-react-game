cfe36aa6ed1fb378f6dfe06a974812a925f9ea9e
5yndr0m <sdilanjana21@gmail.com>
2024-11-22 06:31:00 +0530

    feat: animal images are now visible

--- src/components/animalImages.js
+++ src/components/animalImages.js
@@ -1,7 +1,20 @@
-
+import { animals } from "../assets/AnimalsDb";
 
 export default function AnimalImages() {
   return (
-    <></>
+    <>
+      <h1>Select the Animal</h1>
+      {
+        animals.map((animal) => (
+          <img
+            key={animal.name}
+            src={`/fig/${animal.img}`}
+            alt={animal.name}
+            // onClick={() => handleAnimalClick(animal.name)}
+            style={{ cursor: "pointer", width: "150px", height: "150px", objectFit: "cover" }}
+          />)
+        )
+      }
+    </>
   );
 }
