a243612e5fa7b74fbedaa7c219d125ac0b9e1d8a
5yndr0m <sdilanjana21@gmail.com>
2024-11-22 05:45:50 +0530

    feat: created the file structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import ResultView from './components/resultView';
+import AnimalImages from './components/animalImages';
+import RandomNamePicker from './components/randomNamePicker';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col>Animal Matching Game</Col>
+      </Row>
+      <Row>
+        <Col><ResultView /></Col>
+        <Col><RandomNamePicker /></Col>
+        <Col><AnimalImages /></Col>
+      </Row>
+    </Container>
   );
 }
 
